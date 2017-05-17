4cbf41f13ca2697a84c4a67a2c49544e4e5d1113
obukata <squib02@gmail.com>
2017-05-17 20:51:44 +0900

    jspoPlayer変更に伴う修正

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,7 @@
 javascript:(function(){
-	var jspoTarget = document.getElementById("UlizaMediaPlayback_player");
-	var jspoHeader = document.getElementsByTagName("header");
+	var jspoTarget = document.getElementsByTagName("video")[0];
+	var jspoControlTarget = document.getElementsByClassName("vjs-control-bar")[0];
+	var jspoHeader = document.getElementsByTagName("header")[0];
 
 	jspoTarget.style.position = "fixed";
 	jspoTarget.style.width = "100%";
@@ -8,12 +9,17 @@ javascript:(function(){
 	jspoTarget.style.top = "0";
 	jspoTarget.style.left = "0";
 	jspoTarget.style.zIndex = "1000000";
+	jspoControlTarget.style.position = "fixed";
+	jspoControlTarget.style.bottom = "0";
+	jspoControlTarget.style.left = "0";
+	jspoControlTarget.style.width = "100%";
+	jspoControlTarget.style.zIndex = "1000000";
 	jspoHeader.style.display = "none";
 
 	var jspoChange = document.createElement("div");
 	jspoChange.id = "jspoButton";
-	var jspoCover = document.getElementById("sb-site");
-	jspoChange.innerHTML = "<div id='jspoChange' style='position: fixed;z-index: 99999999;top: 0px;right: 100px;padding: 15px;font-size: 12px;background: rgba(0,0,100,0.5);border-radius: 0 0 5px 5px;display: block;color: #fff;cursor: pointer;'>元に戻す</div>";
+	var jspoCover = document.getElementById("main")[0];
+	jspoChange.innerHTML = "<div id='jspoCover' style='position: fixed;z-index: 100000;top: 0px;left: 0px;right: 0px;bottom: 0px;margin: auto;background: #000;'></div><div id='jspoChange' style='position: fixed;z-index: 99999999;top: 0px;right: 100px;padding: 15px;font-size: 12px;background: rgba(0,0,100,0.5);border-radius: 0 0 5px 5px;display: block;color: #fff;cursor: pointer;'>元に戻す</div>";
 	document.body.insertBefore(jspoChange, jspoCover);
 	jspoChange.onclick = function() {
 		var jspoButton = document.getElementById("jspoButton");
